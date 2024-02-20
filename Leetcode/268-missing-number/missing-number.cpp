7cc15252d3c6391aa0eb2aead7f0b8dd435cc174
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-02-20 07:33:59 +0200

    Time: 8 ms (93.06%) | Memory: 20.4 MB (32.85%) - LeetSync

--- Leetcode/268-missing-number/missing-number.cpp
+++ Leetcode/268-missing-number/missing-number.cpp
@@ -4,7 +4,7 @@ public:
         ios_base::sync_with_stdio(0);
 	    cin.tie(0);
 	    cout.tie(0);
-
-        return (int)((nums.size()*(nums.size()+1)/2)-accumulate(nums.begin(),nums.end(),0));
+        int n  =nums.size();
+        return ((n*(n+1))>>1)-accumulate(nums.begin(),nums.end(),0);
     }
 };
