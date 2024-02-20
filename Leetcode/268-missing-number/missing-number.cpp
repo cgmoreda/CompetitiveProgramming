63b7950810ce94b6ef77c5511980aa06e26f6093
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-02-20 07:32:52 +0200

    Time: 12 ms (74.59%) | Memory: 20.4 MB (32.85%) - LeetSync

--- Leetcode/268-missing-number/missing-number.cpp
+++ Leetcode/268-missing-number/missing-number.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int x=0;
-        for(int i=0;i<nums.size();i++)
-            x^=nums[i]^(i+1);
         
-        return x;
+        return (int)((nums.size()*(nums.size()+1)/2)-accumulate(nums.begin(),nums.end(),0));
     }
 };
