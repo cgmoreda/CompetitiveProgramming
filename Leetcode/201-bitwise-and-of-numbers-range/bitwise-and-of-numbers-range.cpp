7832ccc2d22eb91f124aad8edc5945b3955ff1b2
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-02-21 07:49:31 +0200

    Time: 10 ms (41.24%) | Memory: 8.9 MB (8.30%) - LeetSync

--- Leetcode/201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ Leetcode/201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -1,19 +1,13 @@
-
-
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
        
-        ios_base::sync_with_stdio(NULL);
-        cin.tie(NULL);
-        cout.tie(NULL);
-     
         unsigned int z = left^right;
         while(z&(z-1))
             z&=z-1;
-        if(z){
+        if(z)
             z<<=1,z-=1;
-        }
+        
         return (left&right)&(~z);
     
     }
