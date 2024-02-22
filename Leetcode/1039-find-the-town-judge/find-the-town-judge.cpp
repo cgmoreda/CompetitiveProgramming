857485d9f181fd907af5951eea31c09f8028f125
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-02-22 08:09:40 +0200

    Time: 120 ms (86.35%) | Memory: 64.1 MB (75.39%) - LeetSync

---
+++ Leetcode/1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+ public:
+	int findJudge(int n, vector<vector<int>>& trust)
+	{
+		vector<int> countTrust(n + 1);
+		for (auto& v : trust)
+		{
+			int a = v[0], b = v[1];
+			countTrust[b]++;
+			countTrust[a] = -1e5;
+		}
+		for (int i = 1; i <= n; i++)
+			if (countTrust[i] == n - 1)
+				return i;
+
+		return -1;
+
+	}
+};
