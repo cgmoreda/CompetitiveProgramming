e13f89be59b58536fe82290a8782c14c778647dd
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 01:50:42 +0200

    Time: 29 ms (8.71%) | Memory: 40.3 MB (5.81%) - LeetSync

--- Leetcode/238-product-of-array-except-self/product-of-array-except-self.cpp
+++ Leetcode/238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -7,16 +7,19 @@ public:
         cout.tie(nullptr);
 
         int n =nums.size();
-        vector<int>pref(nums),suf(nums),ret(n,1);
+        vector<int>pref(n,1),suf(n,1),ret(n,1);
         
-        for(int i=n-2;i>=0;i--){
-            suf[i] *=suf[i+1];
-            ret[i]*=suf[i+1];
-        }
+        pref[0] = nums[0];
+        suf[n-1] = nums[n-1];
         for(int i=1;i<n;i++)
         {
-            pref[i] *=pref[i-1];
+            pref[i] =pref[i-1]*nums[i];
             ret[i]*=pref[i-1];
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            suf[i] =suf[i+1]*nums[i];
+            ret[i]*=suf[i+1];
         }
         return ret;
     }
