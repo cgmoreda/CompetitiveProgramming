1d1c6eaedf79320383d9470da4084e88853adacd
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 01:27:06 +0200

    Time: 11 ms (70.57%) | Memory: 22.7 MB (88.43%) - LeetSync

--- Leetcode/605-can-place-flowers/can-place-flowers.cpp
+++ Leetcode/605-can-place-flowers/can-place-flowers.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& f, int n) {
-        int x = 0;
-        for(int i=0;i<f.size();)
+        int x = 0, m = f.size();
+        for(int i=0;i<m;)
         {
             if(f[i])
-            {
                 i+=2;
-            }
-            else if(i+1>=f.size()||!f[i+1]) {
-                x++;
-                i+=2;
-            }
+            else if(i+1>=m||!f[i+1])
+                x++,i+=2;
             else 
                 i++;
         }
