e0b9056024ad567fc4d6d28c6a59e0913cbc55f7
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 01:52:58 +0200

    Time: 16 ms (63.68%) | Memory: 38.2 MB (10.13%) - LeetSync

--- Leetcode/238-product-of-array-except-self/product-of-array-except-self.cpp
+++ Leetcode/238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,26 +1,28 @@
-class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-       
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
+class Solution
+{
+ public:
+	vector<int> productExceptSelf(vector<int>& nums)
+	{
 
-        int n =nums.size();
-        vector<int>pref(n,1),suf(n,1),ret(n,1);
-        
-        pref[0] = nums[0];
-        suf[n-1] = nums[n-1];
-        for(int i=1;i<n;i++)
-        {
-            pref[i] =pref[i-1]*nums[i];
-            ret[i]*=pref[i-1];
-        }
-        
-        for(int i=n-2;i>=0;i--){
-            suf[i] =suf[i+1]*nums[i];
-            ret[i]*=suf[i+1];
-        }
-        return ret;
-    }
+		ios_base::sync_with_stdio(false);
+		cin.tie(nullptr);
+		cout.tie(nullptr);
+
+		int n = nums.size();
+		vector<int> ret(n, 1);
+		int x = nums[0];
+
+		for (int i = 1; i < n; i++)
+		{
+			ret[i] *= x;
+			x *= nums[i];
+		}
+		x = nums[n-1];
+		for (int i = n - 2; i >= 0; i--)
+		{
+			ret[i] *= x;
+			x *= nums[i];
+		}
+		return ret;
+	}
 };
