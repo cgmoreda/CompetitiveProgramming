aee0b239088c37a0c0b956b3cd66e4c487eeda30
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 09:01:53 +0200

    Time: 15 ms (54.12%) | Memory: 19 MB (98.75%) - LeetSync

--- Leetcode/136-single-number/single-number.cpp
+++ Leetcode/136-single-number/single-number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ele = 0;
-        for (int i = 0; i < nums.size(); i++)
-            ele ^= nums[i];
+        for(auto &i:nums)
+           ele ^= i;
         return ele;
     }
 };
