029fa08a0cc1016f2dbc717675c9c18ce178f503
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 02:20:08 +0200

    Time: 10 ms (93.04%) | Memory: 21.6 MB (73.83%) - LeetSync

--- Leetcode/283-move-zeroes/move-zeroes.cpp
+++ Leetcode/283-move-zeroes/move-zeroes.cpp
@@ -1,3 +1,10 @@
+const auto init = [](){
+       std::cin.tie(nullptr);
+       std::cout.tie(nullptr);
+       std::ios_base::sync_with_stdio(false);
+       return 42;
+}();
+
 class Solution {
 public:
     void moveZeroes(vector<int>& v) {
