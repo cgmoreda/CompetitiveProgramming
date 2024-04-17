bb873ac696e0fcacfe4af99cc59f08003ceac2bc
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 03:02:32 +0200

    Time: 23 ms (99.66%) | Memory: 58.7 MB (42.46%) - LeetSync

---
+++ Leetcode/1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,29 @@
+const auto init = [](){
+       std::cin.tie(nullptr);
+       std::cout.tie(nullptr);
+       std::ios_base::sync_with_stdio(false);
+       return 42;
+}();
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int k=1;
+        if(count(nums.begin(),nums.end(),1)==0)
+            return 0;
+        int n = nums.size(),nzeros=0;
+        vector<int>&v =nums;
+        int ans=0;
+        for(int l=0,r=0;r<n;r++)
+        {
+            nzeros+=nums[r]==0;
+            while(nzeros>k)
+            {
+                nzeros-=nums[l]==0;
+                l++;
+            }
+            ans = max(ans,r-l+1);
+        }   
+        
+        return ans-1;
+    }
+};
