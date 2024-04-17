677632d63ef17e4e41fd069f76d5900218661c36
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 05:38:02 +0200

    Time: 12 ms (39.18%) | Memory: 20.8 MB (8.79%) - LeetSync

--- Leetcode/735-asteroid-collision/asteroid-collision.cpp
+++ Leetcode/735-asteroid-collision/asteroid-collision.cpp
@@ -1,35 +1,34 @@
 #define all(v) v.begin(), v.end()
-const auto init = [](){
-       std::cin.tie(nullptr);
-       std::cout.tie(nullptr);
-       std::ios_base::sync_with_stdio(false);
-       return 11;
-}();
-
-
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int> out,st;
+        vector<int> out;
+        stack<int> st;
 
         for (auto& i : asteroids) {
             if (i < 0) {
-                while (st.size() && st.back() < -i)
-                    st.pop_back();
+                while (st.size() && st.top() < -i)
+                    st.pop();
+
                 if (st.empty()) {
                     out.push_back(i);
-                } else if (st.back() == -i) {
-                    st.pop_back();
+                } else if (st.top() == -i) {
+                    st.pop();
                 }
-            } 
-            else {
-                st.push_back(i);
+            } else {
+                st.push(i);
             }
         }
-        
-      
-        for (auto i : st)
-            out.push_back(i);
+        stack<int> st2;
+        while (st.size()) {
+            st2.push(st.top());
+            st.pop();
+        }
+
+        while (st2.size()) {
+            out.push_back(st2.top());
+            st2.pop();
+        }
         return out;
     }
 };
