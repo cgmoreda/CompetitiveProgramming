da19a02f8460e7a20d623cd313e0728e1c65daec
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 05:10:48 +0200

    Time: 67 ms (69.43%) | Memory: 22.9 MB (46.11%) - LeetSync

--- Leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ Leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,4 +1,7 @@
 #define all(v) v.begin(),v.end()
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops", "no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
 
 
 const auto init = [](){
