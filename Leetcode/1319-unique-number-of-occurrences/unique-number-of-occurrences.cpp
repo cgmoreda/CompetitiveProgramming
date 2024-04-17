d95a8a39f32437a6620f3551ba7cd76fa642b56b
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 04:59:40 +0200

    Time: 0 ms (100.00%) | Memory: 11.2 MB (38.91%) - LeetSync

--- Leetcode/1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ Leetcode/1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -3,10 +3,10 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int>mp,mp2;
         for(auto &i:arr)
-         mp[i]++;
-        for(auto [x,y]:mp)
-            if(mp2[y]++)
-            return false;
+            mp[i]++;
+        for(auto [k,f]:mp)
+            if(mp2[f]++)
+                return false;
         return true;   
     }
 };
