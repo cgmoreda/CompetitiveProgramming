870dcc7555dd1761e551522294f9a4d85d34ec58
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 02:25:45 +0200

    Time: 3 ms (47.74%) | Memory: 7.4 MB (93.93%) - LeetSync

--- Leetcode/392-is-subsequence/is-subsequence.cpp
+++ Leetcode/392-is-subsequence/is-subsequence.cpp
@@ -1,19 +1,17 @@
-const auto init = [](){
-       std::cin.tie(nullptr);
-       std::cout.tie(nullptr);
-       std::ios_base::sync_with_stdio(false);
-       return 42;
+const auto init = []() {
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+    return 42;
 }();
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        for(int i=0,j=0;i<s.size();i++,j++)
-        {
-            while(j<t.size()&&s[i]!=t[j])
+    bool isSubsequence(string& s, string& t) {
+        for (int i = 0, j = 0; i < s.size(); i++, j++) {
+            while (j < t.size() && s[i] != t[j])
                 j++;
-            if(s[i]!=t[j])
+            if (s[i] != t[j])
                 return false;
-            
         }
         return true;
     }
