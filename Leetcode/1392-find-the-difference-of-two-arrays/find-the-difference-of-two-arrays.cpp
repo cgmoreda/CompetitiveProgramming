57bf80cef78afa9d9768f2b613f96a511586d26f
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 04:01:54 +0200

    Time: 16 ms (99.19%) | Memory: 29.8 MB (95.75%) - LeetSync

--- Leetcode/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ Leetcode/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -10,23 +10,23 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         
-        int arr1[2][2002]{},arr2[2][2002]{}; 
+        int arr1[2002]{},arr2[2002]{}; 
 
         for(auto &i:nums2)
-         i+=1000, arr2[1][i]++;
+            i+=1000, arr2[i]++;
 
         for(auto &i:nums1)
-           i+=1000, arr1[1][i]++;
+           i+=1000, arr1[i]++;
 
         vector<vector<int>>ret(2);
         for(auto i:nums1)
         {
-            if(arr2[1][i]++==0)
+            if(arr2[i]++==0)
                 ret[0].push_back(i-1000);
         }
         for(auto i:nums2)
         {
-            if(arr1[1][i]++==0)
+            if(arr1[i]++==0)
                 ret[1].push_back(i-1000);
         }
         return ret;
