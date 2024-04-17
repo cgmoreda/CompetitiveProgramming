b10df0d747287d64b4f0318e5278712329687181
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-04-17 03:59:07 +0200

    Time: 11 ms (99.87%) | Memory: 30.2 MB (92.67%) - LeetSync

--- Leetcode/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ Leetcode/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -4,27 +4,30 @@ const auto init = [](){
        std::ios_base::sync_with_stdio(false);
        return 42;
 }();
-
+   
 
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>mp1,mp2;
-        for(auto i:nums2)
-            mp2[i]++;
-        for(auto i:nums1)
-            mp1[i]++;
+        
+        int arr1[2][2002]{},arr2[2][2002]{}; 
+
+        for(auto &i:nums2)
+         i+=1000, arr2[1][i]++;
+
+        for(auto &i:nums1)
+           i+=1000, arr1[1][i]++;
 
         vector<vector<int>>ret(2);
         for(auto i:nums1)
         {
-            if(mp2[i]++==0)
-                ret[0].push_back(i);
+            if(arr2[1][i]++==0)
+                ret[0].push_back(i-1000);
         }
         for(auto i:nums2)
         {
-            if(mp1[i]++==0)
-                ret[1].push_back(i);
+            if(arr1[1][i]++==0)
+                ret[1].push_back(i-1000);
         }
         return ret;
     }
