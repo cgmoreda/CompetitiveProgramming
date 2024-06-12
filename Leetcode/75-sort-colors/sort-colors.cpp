8f800e4927d4b1a50fec81ce569297079c4c62e8
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-06-13 01:35:28 +0300

    Time: 0 ms (100.00%) | Memory: 9.9 MB (34.22%) - LeetSync

--- Leetcode/75-sort-colors/sort-colors.cpp
+++ Leetcode/75-sort-colors/sort-colors.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
       int n0=0,n1=0;
-      n0 = count(nums.begin(),nums.end(),0);
-      n1 = count(nums.begin(),nums.end(),1);
+      for(auto i:nums)
+        n0+=i==0,n1+=i==1;
       for(int i=0;i<nums.size();i++)
         nums[i]=(i>=n0)+(i>=n0+n1);
       
