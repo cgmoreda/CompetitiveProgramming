aa9c96a26b133014619ec13318f4145fe3b679bc
Mohamed Reda <89495212+cgmoreda@users.noreply.github.com>
2024-06-19 01:18:06 +0300

    Time: 60 ms (39.00%) | Memory: 41.1 MB (66.75%) - LeetSync

--- Leetcode/853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ Leetcode/853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        multiset<pair<int,int>>ms;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>ms;
         for(int i=0;i<profit.size();i++)
-            ms.insert({difficulty[i],profit[i]});
+            ms.push({difficulty[i],profit[i]});
         
         sort(worker.begin(),worker.end());
         int p =0,ans=0;
         for(int i=0,j=0;i<worker.size();i++)
         {
-            while(ms.size()&&worker[i]>=ms.begin()->first)
-            {
-                p = max(p,ms.begin()->second);
-                ms.erase(ms.begin());
-            }
+            while(ms.size()&&worker[i]>=ms.top().first)
+                p = max(p,ms.top().second),ms.pop();
+            
             ans+=p;
         }
         return ans;
